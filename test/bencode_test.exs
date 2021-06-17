9e78b81fd03f1b98cc2f0d280962b21367aea092
Andres Yepes <yepes07@gmail.com>
2021-06-16 22:51:24 -0500

    style: fix indentation

--- test/bencode_test.exs
+++ test/bencode_test.exs
@@ -3,26 +3,26 @@ defmodule BencodeTest do
   doctest Bencode
 
   test "Test i10e" do
-	assert Bencode.parse("i10e") == 10
+    assert Bencode.parse("i10e") == 10
   end
 
   test "Test le" do
-	assert Bencode.parse("le") == []
+    assert Bencode.parse("le") == []
   end
 
   test "Test li10ee" do
-	assert Bencode.parse("li10ee") == [10]
+    assert Bencode.parse("li10ee") == [10]
   end
 
   test "Test li10ei15ee" do
-	assert Bencode.parse("li10ei15ee") == [10,15]
+    assert Bencode.parse("li10ei15ee") == [10,15]
   end
 
   test "Test lleli8eee" do
-	assert Bencode.parse("lleli8eee") == [[],[8]]
+    assert Bencode.parse("lleli8eee") == [[],[8]]
   end
 
   # test "Test llei3ei1ee" do
-  # 	assert Bencode.parse("llei3ei1ee") == [[], 3, 1]
+  #   assert Bencode.parse("llei3ei1ee") == [[], 3, 1]
   # end
 end
